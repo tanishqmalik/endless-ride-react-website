4f496c77c88f75612ad26ab8d0fd490f7ce299ad
tanishq <tanishqvideo@gmail.com>
2024-04-30 11:44:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-black font-bold bg-purple-400">hideww there</h1>
+    </>
+    
+  )
+}
+
+export default App
