06235f2124a806461989d41d6a68db9351d2fa61
tanishq <tanishqvideo@gmail.com>
2024-05-14 03:06:37 +0530

    bekar github

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/header_img/header';
 import Navbar from './components/navbar/navbar';
 import Header_content from './components/header_content/header_content';
 import Logo from './components/logo/Logo';
+import Header2 from './components/header2/Header2';
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
     <Logo/>
     {/* </div> */}
     <Header_content/>
-    
+    <Header2/>
     </div>
     </>
   )
---
+++ src/components/header2/Header2.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+function Header2() {
+  return (
+    <div className='flex flex-row justify-evenly flex-wrap gap-5'>
+
+      <div className='flex flex-col relative w-[350px]'>
+        <img src='./assets/header2.1.png'></img>
+
+        <div className='flex flex-col absolute bottom-[250px] left-7 items-start text-white'>
+        <span className='text-[50px]'>
+            $ <span className='text-[70px] font-bold'>100</span>
+        </span>
+        <span>PER PERSON</span>
+        </div>
+
+        <div className='flex flex-col items-start gap-4 pt-5'>
+            <span className='text-black font-bold text-[25px]'>Motorcycle Expeditions</span>
+            <p className='text-start opacity-55'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam recusandae tempora exe
+                rcit
+                ationem, enim molestiae mollitia aut excepturi! Quaerat, maxime fug.
+            </p>
+
+            <button className='bg-red-600 rounded-full w-[30%] text-white p-[8px] mt-5'>BOOK NOW</button>
+        </div>
+       
+      </div>
+
+
+
+      <div className='flex flex-col relative w-[350px]'>
+        <img src='./assets/header2.2.png'></img>
+
+        <div className='flex flex-col absolute bottom-[250px] left-7 items-start text-white'>
+        <span className='text-[50px]'>
+            $ <span className='text-[70px] font-bold'>100</span>
+        </span>
+        <span>PER PERSON</span>
+        </div>
+
+        <div className='flex flex-col items-start gap-4 pt-5'>
+            <span className='text-black font-bold text-[25px]'>Mountains Motorcycle Tours</span>
+            <p className='text-start opacity-55'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam recusandae tempora exe
+                rcit
+                ationem, enim molestiae mollitia aut excepturi! Quaerat, maxime fug.
+            </p>
+
+            <button className='bg-red-600 rounded-full w-[30%] text-white p-[8px] mt-5'>BOOK NOW</button>
+        </div>
+       
+      </div>
+
+
+      <div className='flex flex-col relative w-[350px]'>
+        <img src='./assets/header2.3.png'></img>
+
+        <div className='flex flex-col absolute bottom-[250px] left-7 items-start text-white'>
+        <span className='text-[50px]'>
+            $ <span className='text-[70px] font-bold'>100</span>
+        </span>
+        <span>PER PERSON</span>
+        </div>
+
+        <div className='flex flex-col items-start gap-4 pt-5'>
+            <span className='text-black font-bold text-[25px]'>Mountains Motorcycle Tours</span>
+            <p className='text-start opacity-55'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam recusandae tempora exe
+                rcit
+                ationem, enim molestiae mollitia aut excepturi! Quaerat, maxime fug.
+            </p>
+
+            <button className='bg-red-600 rounded-full w-[30%] text-white p-[8px] mt-5'>BOOK NOW</button>
+        </div>
+       
+      </div>
+
+    </div>
+  )
+}
+
+export default Header2
