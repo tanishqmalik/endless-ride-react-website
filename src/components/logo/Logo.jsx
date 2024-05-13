f641c290474dbbbb1b3a875ed154daec0e0672e2
tanishq <tanishqvideo@gmail.com>
2024-05-13 23:51:46 +0530

    hiii

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -5,9 +5,9 @@ function Logo() {
   return (
     
     <div id="logo" className='flex flex-col  absolute left-[40px] items-center top-5 w-[9%] '>
-      <img src='./assets/logo.png' className='w-[45px] h-[50px] relative top-2 left-6'></img>
-      <code className='text-white font-bold text-[24px] flex'>ENDLESS<span className='text-red-800'>RIDE</span></code>
-      <code className='flex text-white text-[13px] relative bottom-2 w-[120px]'>TAGLINE HERE</code>
+      <img id="logo_image" src='./assets/logo.png' className='w-[45px] h-[50px] relative top-2 left-6'></img>
+      <code id="logo_heading" className='text-white font-bold text-[24px] flex'>ENDLESS<span className='text-red-800'>RIDE</span></code>
+      <code id="logo_heading2" className='flex text-white text-[13px] relative bottom-2 w-[120px]'>TAGLINE HERE</code>
     </div>
   )
 }
